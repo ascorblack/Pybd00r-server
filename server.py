acfb7e8c03ba1162bd595e20dbb0aface53e91a2
Scorpiss <sanchatih2@gmail.com>
2021-12-29 21:10:30 +0300

    Update server.py

--- server.py
+++ server.py
@@ -165,7 +165,7 @@ class Server:
 
 
 if __name__ == '__main__':
-    # os.environ["PORT"] = "5000"
+    os.environ["PORT"] = "80"
     port = int(os.environ.get('PORT', 80))
     print(port)
     server = Server("0.0.0.0", port)
